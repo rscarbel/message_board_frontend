efb8eedab06536b78c8bfc93b1d26e3b5eb64891
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-26 17:32:03 -0400

    Making form and area to display messages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Message from './components/Message'
 
-function App() {
+export default function App() {
+
+  const [state, setState] = useState({
+    messages: [],
+    newMessage: {
+      skill: "",
+      level: "3"
+    }
+  });
+
+  function handleChange(event) {
+    setState(({ message, newMessage }) => ({
+      message,
+      newMessage: {
+        ...newMessage,
+        [event.target.name]: event.target.value
+      }
+
+    }));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    await fetch('https://ga-message-board-backend.herokuapp.com/api/message', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'Application/json'
+      },
+      body: JSON.stringify(state.newMessage)
+    });
+
+    getMessages();
+
+    setState((prevState) => ({
+      ...prevState,
+      newMessage: {
+        username: '',
+        message: ''
+      }
+    }));
+  }
+
+  async function getMessages() {
+    const response = await fetch('https://ga-message-board-backend.herokuapp.com/api/messages');
+    const messages = await response.json();
+
+    setState((prevState) => ({
+      messages,
+      newMessage: prevState.newMessage
+    }));
+  }
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Message Board</h1>
+      {state.message.map((item) => (
+          <Message username={item.username} content={item.message} />
+      ))}
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Username</span>
+          <input name='username' type='text' value={state.newMessage.skill} onChange={handleChange} />
+        </label>
+        <label>
+          <span>Message</span>
+          <textarea name='username' value={state.newMessage.skill} onChange={handleChange} />
+        </label>
+        <button>Send</button>
+      </form>
+    </section>
   );
 }
-
-export default App;
