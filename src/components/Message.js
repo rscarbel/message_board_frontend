6d02f2812531a12b130b25b6842443132115596e
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-27 09:23:00 -0400

    Fixed time format

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,41 @@
 const Message = (props) => {
   const convertISOtoRegularTime = (ISOdate) => {
     let date = new Date(ISOdate);
+    let am = true
     let year = date.getFullYear()-2000;
+
     let month = date.getMonth() + 1;
-    let time = `${date.getHours()}:${date.getSeconds()}`
-    return `${month}/${year}⠀⠀⠀${time}`
+    if (month.toString().length === 1) {
+      month = `0${month}`
+    }
+    let day = date.getDate()
+    if (day.toString().length === 1) {
+      day = `0${day}`
+    }
+
+    let hour = date.getHours()
+    if (hour > 12) {
+      am = false;
+      hour = hour-12
+    }
+    if (hour === 0) {
+      hour = 12
+    }
+
+    let minute = date.getMinutes()
+    if (minute.toString().length === 1) {
+      minute = `0${minute}`
+    }
+
+    let time = `${month}/${day}/${year}⠀⠀⠀${hour}:${minute}`
+
+    if (am) {
+      time = `${time} am`
+    } else {
+      time = `${time} pm`
+    }
+
+    return time
   }
 
   return <div className='message'>
