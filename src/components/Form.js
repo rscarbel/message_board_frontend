bc71ae67f4d48de0f6ea75c66efa63458aacd0a1
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-27 12:16:13 -0400

    Check inputs for bad words

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import BadWords from '../scripts/BadWords';
+
 const Form = (props) => {
 
   const [state, setState] = useState({
-      username: "",
-      message: ""
+      username: '',
+      message: ''
     });
 
   function handleChange(event) {
@@ -18,19 +20,25 @@ const Form = (props) => {
   }
   async function handleSubmit(event) {
     event.preventDefault();
+    if (state.newMessage.username === undefined) {
+      state.newMessage.username = 'anonymous'
+    }
+    if (state.newMessage.message.match(BadWords) || state.newMessage.username.match(BadWords)) {
+      alert('You cannot post swear words!')
+    } else {
+      await fetch('https://ga-message-board-backend.herokuapp.com/api/message', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'Application/json'
+        },
+        body: JSON.stringify(state.newMessage)
+      });
 
-    await fetch('https://ga-message-board-backend.herokuapp.com/api/message', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'Application/json'
-      },
-      body: JSON.stringify(state.newMessage)
-    });
-
-    setState((prevState) => ({
-        username: '',
-        message: ''
-    }));
+      setState((prevState) => ({
+          username: '',
+          message: ''
+      }));
+    }
   }
   return <>
   <form onSubmit={handleSubmit}>
