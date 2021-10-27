e1b932f159a783a56cc4cd16df86fde9cecac350
Ryan Scarbel <ryanscarbel@gmail.com>
2021-10-26 20:30:49 -0400

    Added function to display time of post

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,17 @@
 const Message = (props) => {
-  return <>
-  <h3>{props.username}</h3>
+  const convertISOtoRegularTime = (ISOdate) => {
+    let date = new Date(ISOdate);
+    let year = date.getFullYear()-2000;
+    let month = date.getMonth() + 1;
+    let time = `${date.getHours()}:${date.getSeconds()}`
+    return `${month}/${year}⠀⠀⠀${time}`
+  }
+
+  return <div className='message'>
+  <h3 className='messageTitle'>{props.username}</h3>
   <p>{props.content}</p>
-  </>
+  <p className='time'>{convertISOtoRegularTime(props.time)}</p>
+  </div>
 }
 
 export default Message;
